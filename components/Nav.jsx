be0f6dd9aab7606bf3bacb049c97c9d395c06683
Ryder Warbrick <ryderwarbrick@onlydwarfs.com>
2026-04-20 09:07:30 -0700

    Update components/Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,25 +15,28 @@ const Nav = () => {
 
   return (
     <nav className={`od-nav ${scrolled ? 'scrolled' : ''}`}>
-      <a href="index.html" className="od-nav-logo">
-        <img src="assets/OnlyDwarfsLOGOText.svg" alt="OnlyDwarfs" className="od-nav-logo-img" />
-      </a>
-      <ul className="od-nav-links">
-        <li><a href={href('#services')}>Services</a></li>
-        <li><a href={href('#talent')}>Roster</a></li>
-        <li><a href={href('#process')}>Process</a></li>
-        <li><a href="divas.html">Divas Show</a></li>
-        <li>
-          <a href="intake.html" className="od-nav-cta">
-            <span className="od-nav-cta-dot" /> Book now
-          </a>
-        </li>
-        <li>
-          <a href="hub.html" className="od-nav-staff" title="Staff portal">
-            ⚙
-          </a>
-        </li>
-      </ul>
+      <div className="od-nav-inner-wrap">
+        <ul className="od-nav-links od-nav-links-left">
+          <li><a href="services.html">Services</a></li>
+          <li><a href="roster.html">Roster</a></li>
+          <li><a href={href('#process')}>Process</a></li>
+        </ul>
+        <a href="index.html" className="od-nav-logo">
+          <img src="assets/OnlyDwarfsLOGOText.svg" alt="OnlyDwarfs" className="od-nav-logo-img" />
+        </a>
+        <ul className="od-nav-links od-nav-links-right">
+          <li><a href="divas.html">Divas Show</a></li>
+          <li><a href="performer-login.html">Login</a></li>
+          <li>
+            <a href="intake.html" className="od-nav-cta">
+              <span className="od-nav-cta-dot" /> Book now
+            </a>
+          </li>
+          <li>
+            <a href="hub.html" className="od-nav-staff" title="Staff portal">⚙</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
